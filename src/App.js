b15354a94ba55cdd5a9ac718359e6f12e8c6a96d
Nkosinathi Milanzi <nathilanziv@gmail.com>
2024-10-24 15:25:17 +0200

    Update routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './Components/Home';
+import About from './Components/About';
+import VisionPlan from './Components/VisionPlan';
+import Objectives from './Components/Objectives';
+import Divisions from './Components/Divisions';
+import Leadership from './Components/Leadership';
+import Contact from './Components/Contact';
+import Layout from './Components/Layout';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path="/vision-plan" element={<VisionPlan />} />
+          <Route path="/objectives" element={<Objectives />} />
+          <Route path="/divisions" element={<Divisions />} />
+          <Route path="/leadership" element={<Leadership />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Layout>
+    </Router>
   );
 }
 
