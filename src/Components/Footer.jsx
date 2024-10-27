22b6fa6a5679792e603648df7c5edd7f4d185655
Koketso Moilwe <koketsomoilwe2@gmail.com>
2024-10-27 10:58:06 +0200

    The footer background is set to #1B5538 with links that use #397D5A on hover for consistency

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,17 +3,17 @@ import { Link } from 'react-router-dom';
 
 function Footer() {
   return (
-    <footer className="bg-gray-800 text-white py-6 mt-12">
+    <footer className="bg-[#1B5538] text-white py-6 mt-12">
       <div className="container mx-auto px-6 text-center">
         <p className="mb-4">&copy; 2024 OOKKR - All Rights Reserved</p>
-        <div className="flex justify-center space-x-4">
-          <Link to="/" className="hover:underline">Home</Link>
-          <Link to="/about" className='hover:underline'>About</Link>
-          <Link to="/vision-plan" className="hover:underline">VisionPlan 2035</Link>
-          <Link to="/Objective" className="hover:underline">Objective</Link>
-          <Link to="/Division" className="hover:underline">Division</Link>
-          <Link to="/Leadership" className="hover:underline">Leadership</Link>
-          <Link to="/contact" className="hover:underline">Contact</Link>
+        <div className="flex justify-center space-x-4 text-[#397D5A]">
+          <Link to="/" className="hover:text-white">Home</Link>
+          <Link to="/about" className='hover:text-white'>About</Link>
+          <Link to="/vision-plan" className="hover:text-white">VisionPlan 2035</Link>
+          <Link to="/objectives" className="hover:text-white">Objectives</Link>
+          <Link to="/divisions" className="hover:text-white">Divisions</Link>
+          <Link to="/leadership" className="hover:text-white">Leadership</Link>
+          <Link to="/contact" className="hover:text-white">Contact</Link>
         </div>
       </div>
     </footer>
